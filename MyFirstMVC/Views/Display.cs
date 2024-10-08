5c2ef7ca1fc323d2c14072f1b57a67488eacd705
1212-7 <1212-7@DESKTOP-EQ300P5>
2024-10-08 13:10:10 +0300

    Add project files.

---
+++ MyFirstMVC/Views/Display.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMVC.Views
+{
+    public class Display
+    {
+        public double Amount { get; set; }
+        public double Percent { get; set; }
+        public double TipSum { get; set; }
+        public double TotalSum { get; set; }
+        public void Input()
+        {
+            Console.Write("Vavedete sumata na smetkata:");
+            double suma = double.Parse(Console.ReadLine());
+            this.Amount = suma;
+            Console.Write("Vavedete procent na kelnera:");
+            double percent = double.Parse(Console.ReadLine());
+            this.Percent = percent;
+        }
+        public void Output() 
+        {
+            Console.WriteLine($"Bakshishat za servityora e:{this.TipSum} lv.");
+            Console.WriteLine($"Sumata za plashtane e:{this.TotalSum} lv.");
+
+        }
+    }
+}
